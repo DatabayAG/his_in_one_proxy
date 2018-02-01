0729ef9b355df830400f2f7ad7e0f3559cd2a790
Guido Vollbach <gvollbach@databay.de>
2018-02-01 17:16:31 +0100

    role string

--- src/Soap/Interactions/JsonBuilder.php
+++ src/Soap/Interactions/JsonBuilder.php
@@ -161,7 +161,7 @@ class JsonBuilder
 					$person->personIDtype = GlobalSettings::getInstance()->getPersonIdType();
 					$person->role		  = $user['role'];
 					$group_element        = new \stdClass();
-					$group_element->num   = $group_id;
+					$group_element->id    = $group_id;
 					$group_element->role  = $group['role'];
 					$group_container[]    = $group_element;
 				}
