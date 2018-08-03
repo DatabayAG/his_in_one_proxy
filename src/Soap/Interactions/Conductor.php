5f1f4a7fb90d970774cfc0ec6d8a0b816c6a46b5
Guido Vollbach <gvollbach@databay.de>
2018-08-03 09:10:49 +0200

    code reformatting

--- src/Soap/Interactions/Conductor.php
+++ src/Soap/Interactions/Conductor.php
@@ -31,7 +31,7 @@ class Conductor
 	 * @var
 	 */
 	protected $course_of_studies;
-	
+
 	/**
 	 * @var
 	 */
@@ -62,38 +62,29 @@ class Conductor
 	public function __construct($term = null, $year = null, $log = null)
 	{
 		DataCache::getInstance();
-		if($log == null)
-		{
+		if ($log == null) {
 			$this->log = new Log();
-		}
-		else
-		{
+		} else {
 			$this->log = $log;
 		}
 
-		if($term == null || $year == null)
-		{
-			if(GlobalSettings::getInstance()->getActualTermId() == null || GlobalSettings::getInstance()->getActualTermYear() == null)
-			{
-				$term_value		= $this->getCurrentTerm();
-				$this->term_id	= $term_value->getTermNumber();
-				$this->year		= $term_value->getYear();
+		if ($term == null || $year == null) {
+			if (GlobalSettings::getInstance()->getActualTermId() == null || GlobalSettings::getInstance()->getActualTermYear() == null) {
+				$term_value    = $this->getCurrentTerm();
+				$this->term_id = $term_value->getTermNumber();
+				$this->year    = $term_value->getYear();
+			} else {
+				$this->term_id = GlobalSettings::getInstance()->getActualTermId();
+				$this->year    = GlobalSettings::getInstance()->getActualTermYear();
 			}
-			else
-			{
-				$this->term_id	= GlobalSettings::getInstance()->getActualTermId();
-				$this->year		= GlobalSettings::getInstance()->getActualTermYear();
-			}
-		}
-		else
-		{
-			$this->term_id	= $term;
-			$this->year		= $year;
+		} else {
+			$this->term_id = $term;
+			$this->year    = $year;
 		}
 		DataCache::getInstance()->setLog($this->log);
-		$this->course_of_studies = array();
+		$this->course_of_studies     = array();
 		$this->course_of_studies_org = array();
-		$this->data_printer = new DataPrinter();
+		$this->data_printer          = new DataPrinter();
 	}
 
 	/**
@@ -104,8 +95,7 @@ class Conductor
 	protected function getCourseOfStudyDetails($cos_lid, $cos_already)
 	{
 		$course_of_study = DataCache::getInstance()->getCourseOfStudyService()->getCourseOfStudyById($cos_lid);
-		if($course_of_study != null)
-		{
+		if ($course_of_study != null) {
 			$this->course_of_studies_org[$course_of_study->getOrgUnitLid()] = $course_of_study;
 			$this->course_of_studies[$course_of_study->getLid()]            = $course_of_study;
 			$cos_already[$cos_lid]                                          = $course_of_study;
@@ -120,28 +110,22 @@ class Conductor
 	{
 		$year               = $this->year;
 		$term_type_value_id = $this->term_id;
-		$service = DataCache::getInstance()->getCourseCatalogService();
-		$term_id = $service->getRootIdOfTerm($year, $term_type_value_id);
-		$catalog_leaf = null;
+		$service            = DataCache::getInstance()->getCourseCatalogService();
+		$term_id            = $service->getRootIdOfTerm($year, $term_type_value_id);
+		$catalog_leaf       = null;
 
-		if($term_id != null && $term_id != '')
-		{
+		if ($term_id != null && $term_id != '') {
 			$this->log->debug(sprintf('Found term id with value %s.', $term_id));
 			$catalog_leaf = $service->getCourseCatalogLeaf($term_id);
 			$this->data_printer->printCourseCatalog($catalog_leaf);
 
 			$this->log->info($this->data_printer->depth . ' Max Depth.');
-			if($catalog_leaf != null && $catalog_leaf->getId() != null )
-			{
+			if ($catalog_leaf != null && $catalog_leaf->getId() != null) {
 				$this->log->debug(sprintf('Found catalog leaf with id %s.', $catalog_leaf->getId()));
-			}
-			else
-			{
+			} else {
 				$this->log->warning(sprintf('No catalog leaf found for term id %s.', $term_id));
 			}
-		}
-		else
-		{
+		} else {
 			$this->log->warning(sprintf('No term id found for year %s and term type value id.', $year, $term_type_value_id));
 		}
 		return $catalog_leaf;
@@ -149,28 +133,25 @@ class Conductor
 
 	public function getAllLecturesForThisTerm()
 	{
-		$services           = DataCache::getInstance();
-		$cos_map            = $services->getCourseOfStudyService()->findCourseOfStudy();
-		$cos_already        = array();
+		$services    = DataCache::getInstance();
+		$cos_map     = $services->getCourseOfStudyService()->findCourseOfStudy();
+		$cos_already = array();
 
 		$year               = $this->year;
 		$term_type_value_id = $this->term_id;
 
 		$unit_list = DataCache::getInstance()->getCourseInterfaceService()->findUnit($term_type_value_id, $year);
 
-		if($unit_list != null && $unit_list->getSizeOfContainer() > 0)
-		{
+		if ($unit_list != null && $unit_list->getSizeOfContainer() > 0) {
 			$this->log->info(sprintf('Starting looking for all lectures in term id %s and year %s.',
 				$term_type_value_id, $year));
 
 			$units = $this->startHandlingUnitList($unit_list, $cos_map, $cos_already);
 			$this->finishHandlingUnits($units);
 
-			$this->log->info(sprintf('Unit relevant for export %s. Unit irrelevant for export %s', 
+			$this->log->info(sprintf('Unit relevant for export %s. Unit irrelevant for export %s',
 				$services->getRelevantForExport(), $services->getIrRelevantForExport()));
-		}
-		else
-		{
+		} else {
 			$this->log->warning(sprintf('No unit ids found for year %s and term type value id.', $year, $term_type_value_id));
 		}
 	}
@@ -180,9 +161,9 @@ class Conductor
 	 */
 	public function getLectureByUnitIdForTerm($unit_id)
 	{
-		$services           = DataCache::getInstance();
-		$cos_map            = $services->getCourseOfStudyService()->findCourseOfStudy();
-		$cos_already        = array();
+		$services    = DataCache::getInstance();
+		$cos_map     = $services->getCourseOfStudyService()->findCourseOfStudy();
+		$cos_already = array();
 
 		$unit_list = new UnitIdList();
 		$unit_list->appendUnitId($unit_id);
@@ -193,29 +174,24 @@ class Conductor
 
 	/**
 	 * @param UnitIdList $unit_list
-	 * @param $cos_map
-	 * @param $cos_already
+	 * @param            $cos_map
+	 * @param            $cos_already
 	 * @return array
 	 * @throws \Exception
 	 */
 	protected function startHandlingUnitList($unit_list, $cos_map, $cos_already)
 	{
-		$units = array();
+		$units              = array();
 		$year               = $this->year;
 		$term_type_value_id = $this->term_id;
 
-		foreach($unit_list->getUnitId() as $unit_id)
-		{
-			if(DataCache::getInstance()->getCourseInterfaceService()->doesCombinationForCourseExist($unit_id, $term_type_value_id, $year))
-			{
+		foreach ($unit_list->getUnitId() as $unit_id) {
+			if (DataCache::getInstance()->getCourseInterfaceService()->doesCombinationForCourseExist($unit_id, $term_type_value_id, $year)) {
 				$unit = $this->gatherUnitDetails($unit_id, $cos_map, $cos_already);
-				if($unit != null)
-				{
+				if ($unit != null) {
 					$units[] = $unit;
 				}
-			}
-			else
-			{
+			} else {
 				DataCache::getInstance()->incrementIrrelevantForExport();
 			}
 		}
@@ -232,22 +208,19 @@ class Conductor
 	 */
 	protected function gatherUnitDetails($unit_id, $cos_map, $cos_already)
 	{
-		$services	= DataCache::getInstance();
-		$unit		= $services->getCourseInterfaceService()->readUnit($unit_id);
+		$services           = DataCache::getInstance();
+		$unit               = $services->getCourseInterfaceService()->readUnit($unit_id);
 		$year               = $this->year;
 		$term_type_value_id = $this->term_id;
 
-		if($unit->getId() != null)
-		{
+		if ($unit->getId() != null) {
 			$services->getCourseInterfaceService()->getCombinationForCourse($unit, $term_type_value_id, $year);
 
-			if($unit->getSizeOfCourseMappingContainer() >= 1 && $unit->getCourseMappingContainer()[0]->getELearningSystemId() != null)
-			{
+			if ($unit->getSizeOfCourseMappingContainer() >= 1 && $unit->getCourseMappingContainer()[0]->getELearningSystemId() != null) {
 				$this->log->debug(sprintf('Read modules for unit %s.', $unit->getId()));
 
 				$modules_list = $services->getCourseInterfaceService()->getModulesForUnit($unit->getId());
-				if($modules_list != null && $modules_list->getSizeOfContainer() > 0)
-				{
+				if ($modules_list != null && $modules_list->getSizeOfContainer() > 0) {
 					$this->log->debug(sprintf('Found %s modules for unit %s.', $modules_list->getSizeOfContainer(), $unit->getId()));
 					$this->readDetailsForOrgUnits($unit, $modules_list);
 				}
@@ -264,17 +237,15 @@ class Conductor
 
 	/**
 	 * @param Unit $unit
-	 * @param $unit_id_list
+	 * @param      $unit_id_list
 	 * @throws \Exception
 	 */
 	protected function readDetailsForOrgUnits($unit, $unit_id_list)
 	{
 		$org_units = array();
-		foreach($unit_id_list->getUnitIdContainer() as $module_unit_id)
-		{
+		foreach ($unit_id_list->getUnitIdContainer() as $module_unit_id) {
 			$module = DataCache::getInstance()->getCourseInterfaceService()->readUnit($module_unit_id);
-			if($module->getId() != null)
-			{
+			if ($module->getId() != null) {
 				$org_units[] = $this->getOrgUnitDetails($module);
 			}
 		}
@@ -282,8 +253,8 @@ class Conductor
 
 	/**
 	 * @param Unit $unit
-	 * @param $cos_map
-	 * @param $cos_already
+	 * @param      $cos_map
+	 * @param      $cos_already
 	 */
 	protected function readCourseOfStudyForUnit($unit, $cos_map, $cos_already)
 	{
@@ -306,16 +277,13 @@ class Conductor
 	 */
 	protected function getOrgUnitDetails($module)
 	{
-		$services           = DataCache::getInstance();
+		$services = DataCache::getInstance();
 		$this->log->debug(sprintf('Read details for module %s.', $module->getLid()));
 		$org_units = array();
 		$this->log->debug(sprintf('Size of org unit container %s.', $module->getSizeOfOrgUnitContainer()));
-		if($module->getSizeOfOrgUnitContainer() > 0)
-		{
-			foreach($module->getOrgUnitsContainer() as $org_unit)
-			{
-				if(!array_key_exists($org_unit->getLid(), $org_units))
-				{
+		if ($module->getSizeOfOrgUnitContainer() > 0) {
+			foreach ($module->getOrgUnitsContainer() as $org_unit) {
+				if (!array_key_exists($org_unit->getLid(), $org_units)) {
 					$org_units[$org_unit->getLid()] = $services->getOrgUnitService()->readOrgUnit($org_unit->getLid());
 					$this->log->debug(sprintf('Read details for orgunit lid %s.', $org_unit->getLid()));
 				}
@@ -332,36 +300,27 @@ class Conductor
 	 */
 	protected function getDetailOfCourseOfStudy($unit, $cos_map, $cos_already)
 	{
-		$services	= DataCache::getInstance();
+		$services = DataCache::getInstance();
 
-		if(!array_key_exists($unit->getId(), $this->course_of_studies))
-		{
+		if (!array_key_exists($unit->getId(), $this->course_of_studies)) {
 			$course_ids = $services->getCourseInterfaceService()->getCourseOfStudiesForUnit($unit->getId());
 			//Todo: check whats wrong here, only a quick fix
-			if($course_ids !== null)
-			{
-                foreach($course_ids->getCourseOfStudyIdContainer() as $course_id)
-                {
-                    if(array_key_exists($course_id, $cos_map))
-                    {
-                        $cos_lid = $cos_map[$course_id];
-                        if(!array_key_exists($cos_lid, $cos_already))
-                        {
-                            $this->log->debug(sprintf('Read details for course of study %s.', $cos_lid));
-                            $cos_already = $this->getCourseOfStudyDetails($cos_lid, $cos_already);
-                        }
-                        else
-                        {
-                            $this->log->debug(sprintf('Course of study %s was already found.', $cos_lid));
-                        }
-                    }
-                    else
-                    {
-                        $this->log->warning(sprintf('Course of study with id %s was not found in map.', $course_id));
-                    }
-                }
-                $unit->setCourseOfStudies($cos_already);
-            }
+			if ($course_ids !== null) {
+				foreach ($course_ids->getCourseOfStudyIdContainer() as $course_id) {
+					if (array_key_exists($course_id, $cos_map)) {
+						$cos_lid = $cos_map[$course_id];
+						if (!array_key_exists($cos_lid, $cos_already)) {
+							$this->log->debug(sprintf('Read details for course of study %s.', $cos_lid));
+							$cos_already = $this->getCourseOfStudyDetails($cos_lid, $cos_already);
+						} else {
+							$this->log->debug(sprintf('Course of study %s was already found.', $cos_lid));
+						}
+					} else {
+						$this->log->warning(sprintf('Course of study with id %s was not found in map.', $course_id));
+					}
+				}
+				$unit->setCourseOfStudies($cos_already);
+			}
 		}
 	}
 
@@ -376,14 +335,12 @@ class Conductor
 		$builder = new JsonBuilder();
 		$courses = $builder::convertUnitsToArray($units);
 		$queue   = new SimpleQueue();
-		foreach($courses as $course)
-		{
+		foreach ($courses as $course) {
 			$queue->push(QueueConstants::SERVICE_QUEUE, json_encode($course), QueueConstants::PUBLISH_COURSE_TO_ECS, $course->elearning_sys_string);
 		}
 
 		$persons = $builder::getPersonPlanElements();
-		foreach($persons as $lecture_id => $plan_elements)
-		{
+		foreach ($persons as $lecture_id => $plan_elements) {
 			$e_learning_id = $builder->getElearningSystemStringFromPlanElementId($lecture_id);
 			$queue->push(QueueConstants::SERVICE_QUEUE, json_encode($plan_elements), QueueConstants::PUBLISH_MEMBERS_TO_ECS, $e_learning_id);
 		}
@@ -396,12 +353,12 @@ class Conductor
 	{
 		$org_unit_service = DataCache::getInstance()->getOrgUnitService();
 
-		$lid = $org_unit_service->getUniversityLid();
+		$lid           = $org_unit_service->getUniversityLid();
 		$org_unit_root = $org_unit_service->getOrgUnitWithChildren($lid);
 
 		$this->data_printer->printOrgUnit($org_unit_root, $this->course_of_studies_org);
 		$this->log->debug('Got institution structure submitting to ecs...');
-		$builder = new JsonBuilder();
+		$builder   = new JsonBuilder();
 		$structure = $builder::convertOrgUnitsToJson($org_unit_root);
 
 		$queue = new SimpleQueue();
