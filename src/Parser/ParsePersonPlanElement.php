2470ca685cadb81719cd60589c38b9c0db2d2bc3
Guido Vollbach <gvollbach@databay.de>
2018-08-08 16:28:17 +0200

    revert2

--- src/Parser/ParsePersonPlanElement.php
+++ src/Parser/ParsePersonPlanElement.php
@@ -21,10 +21,6 @@ class ParsePersonPlanElement extends SimpleXmlParser
 		foreach($xml->personPlanelements as $value)
 		{
 			$person_plan_element = new DataModel\PersonPlanElement();
-			if(property_exists($value, 'personPlanelement'))
-			{
-				$value = $value->personPlanelement;
-			}
 			if(isset($value->planelementId) && $value->planelementId != null && $value->planelementId != '')
 			{
 				$person_plan_element->setPlanElementId($value->planelementId);
