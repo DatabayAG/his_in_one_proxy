45980e391bf4a11f63823c851ef6929882040ac6
Guido Vollbach <gvollbach@databay.de>
2025-10-30 11:52:07 +0100

    Minor refactoring

--- src/EcsLocal/Routes/index.php
+++ src/EcsLocal/Routes/index.php
@@ -48,18 +48,18 @@ $app->addErrorMiddleware(true, true, true);
 $app->addBodyParsingMiddleware();
 $logging = new Log();
 $db = new QueueDatabase();
-$local_functions = new EcsLocalFunctions();
+$ecs_local_functions = new EcsLocalFunctions();
 
-$app->get(SLASH, function (Request $request, Response $response) use ($logging, $local_functions) {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get(SLASH, function (Request $request, Response $response) use ($logging, $ecs_local_functions) {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
     $response->getBody()->write('Nothing to see here.');
     return $response;
 });
 
-$app->get(SYS_EVENTS_FIFO, function (Request $request, Response $response) use ($db, $logging, $local_functions)  {
-    $valid_participant = $local_functions->isValidParticipant();
+$app->get(SYS_EVENTS_FIFO, function (Request $request, Response $response) use ($db, $logging, $ecs_local_functions)  {
+    $valid_participant = $ecs_local_functions->isValidParticipant();
     if($valid_participant->isInvalidAuth()) {
         return $response;
     }
@@ -69,21 +69,21 @@ $app->get(SYS_EVENTS_FIFO, function (Request $request, Response $response) use (
     if(count($data) > 0) {
         foreach ($data as $row) {
             if (isset($row['service_id']) && $row['service_id'] > 0 && isset($row['cmd'])) {
-                $value[] = $local_functions->convertEventsFifoData($row);
+                $value[] = $ecs_local_functions->convertEventsFifoData($row);
             }
         }
         $logging->info(sprintf('Transmitted fifo event with following data: "status" => %s, "ressource" => %s',
             $value[0]['status'] ?? '', $value[0]['ressource'] ?? ''));
     }
 
-    $json = $local_functions->jsonEncodeWrapper($value);
+    $json = $ecs_local_functions->jsonEncodeWrapper($value);
     $response->getBody()->write(print_r($json, true));
 
     return $response->withHeader('Content-Type', 'application/json');
 });
 
-$app->post(SYS_EVENTS_FIFO, function (Request $request, Response $response) use ($db, $logging, $local_functions)  {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->post(SYS_EVENTS_FIFO, function (Request $request, Response $response) use ($db, $logging, $ecs_local_functions)  {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
 
@@ -98,64 +98,64 @@ $app->post(SYS_EVENTS_FIFO, function (Request $request, Response $response) use 
     return $response->withHeader('Content-Type', 'application/json');
 });
 
-$app->get(SYS_MEMBERSHIPS, function (Request $request, Response $response) use ($db, $logging, $local_functions)  {
-    $valid_participant = $local_functions->isValidParticipant();
+$app->get(SYS_MEMBERSHIPS, function (Request $request, Response $response) use ($db, $logging, $ecs_local_functions)  {
+    $valid_participant = $ecs_local_functions->isValidParticipant();
     if($valid_participant->isInvalidAuth()) {
         return $response;
     }
 
-    $memberships_json = $local_functions->jsonEncodeWrapper($local_functions->getMemberships($valid_participant));
+    $memberships_json = $ecs_local_functions->jsonEncodeWrapper($ecs_local_functions->getMemberships($valid_participant));
     $response->getBody()->write($memberships_json);
 
     return $response->withHeader('Content-Type', 'application/json');
 });
 
-$app->get(CC_COURSES, function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions)  {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get(CC_COURSES, function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions)  {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
 
     if(isset($args['id'])) {
         $courseId = $args['id'];
         $data = $db->select($courseId);
-        $json = $local_functions->jsonEncodeWrapper($data);
+        $json = $ecs_local_functions->jsonEncodeWrapper($data);
         $response->getBody()->write($json);
         return $response;
     }
 
-    $data = $db->selectQueueWaitingEntries($local_functions->isValidParticipant()->getPid(), COURSES);
-    $json = $local_functions->jsonEncodeWrapper($data);
+    $data = $db->selectQueueWaitingEntries($ecs_local_functions->isValidParticipant()->getPid(), COURSES);
+    $json = $ecs_local_functions->jsonEncodeWrapper($data);
     $response->getBody()->write($json);
     return $response->withHeader('Content-Type', 'text/uri-list');
 });
 
-$app->get('/campusconnect/courses/{id}/details', function (Request $request, Response $response, $args) use ($db, $logging, $local_functions)  {
-    $valid_participant = $local_functions->isValidParticipant();
+$app->get('/campusconnect/courses/{id}/details', function (Request $request, Response $response, $args) use ($db, $logging, $ecs_local_functions)  {
+    $valid_participant = $ecs_local_functions->isValidParticipant();
     if($valid_participant->isInvalidAuth()) {
         return $response;
     }
 
     $courseId = (int) $args['id'];
-    $payload = $local_functions->getPayloadForCourses($valid_participant, $courseId);
-    $memberships_json = $local_functions->jsonEncodeWrapper($payload);
+    $payload = $ecs_local_functions->getPayloadForCourses($valid_participant, $courseId);
+    $memberships_json = $ecs_local_functions->jsonEncodeWrapper($payload);
     $response->getBody()->write($memberships_json);
     $logging->info(sprintf('Sent message details for %s', CC_COURSES . '/details'));
 
     return $response->withHeader('Content-Type', 'application/json');
 });
 
-$app->get('/campusconnect/course_members', function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions) {
-    if ($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get('/campusconnect/course_members', function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions) {
+    if ($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
-    $data = $db->selectQueueWaitingEntries($local_functions->isValidParticipant()->getPid(), COURSE_MEMBERS);
-    $json = $local_functions->jsonEncodeWrapper($data);
+    $data = $db->selectQueueWaitingEntries($ecs_local_functions->isValidParticipant()->getPid(), COURSE_MEMBERS);
+    $json = $ecs_local_functions->jsonEncodeWrapper($data);
     $response->getBody()->write($json);
     return $response->withHeader('Content-Type', 'text/uri-list');
 });
 
-$app->get('/campusconnect/course_members/[{id}]', function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions) {
-    if ($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get('/campusconnect/course_members/[{id}]', function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions) {
+    if ($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
 
@@ -166,7 +166,7 @@ $app->get('/campusconnect/course_members/[{id}]', function (Request $request, Re
         if ($membersId > 0) {
             $data = $db->select($membersId);
         }
-        $json = $local_functions->jsonEncodeWrapper($data);
+        $json = $ecs_local_functions->jsonEncodeWrapper($data);
         $response->getBody()->write($json);
         return $response;
     }
@@ -175,55 +175,55 @@ $app->get('/campusconnect/course_members/[{id}]', function (Request $request, Re
     return $response;
 });
 
-$app->get('/campusconnect/course_members/{id}/details', function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions)  {
-    $valid_participant = $local_functions->isValidParticipant();
+$app->get('/campusconnect/course_members/{id}/details', function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions)  {
+    $valid_participant = $ecs_local_functions->isValidParticipant();
     if($valid_participant->isInvalidAuth()) {
         return $response;
     }
 
     $courseId = (int) $args['id'];
-    $payload = $local_functions->getPayloadForCourseMembers($valid_participant, $courseId);
-    $json = $local_functions->jsonEncodeWrapper($payload);
+    $payload = $ecs_local_functions->getPayloadForCourseMembers($valid_participant, $courseId);
+    $json = $ecs_local_functions->jsonEncodeWrapper($payload);
     $response->getBody()->write($json);
     $logging->info(sprintf('Sent message details for %s', '/campusconnect/course_members/{id}/details'));
 
     return $response->withHeader('Content-Type', 'application/json');
 });
 
-$app->get('/campusconnect/categories', function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions)  {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get('/campusconnect/categories', function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions)  {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
     return $response;
 });
 
-$app->get('/campusconnect/categories/{id}/details', function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions)  {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get('/campusconnect/categories/{id}/details', function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions)  {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
     return $response;
 });
 
-$app->get(CC_COURSE_LINKS, function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions)  {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get(CC_COURSE_LINKS, function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions)  {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
 
     $data = [];
-    $json = $local_functions->jsonEncodeWrapper($data);
+    $json = $ecs_local_functions->jsonEncodeWrapper($data);
     $response->getBody()->write($json);
 
     return $response;
 });
 
-$app->post(CC_COURSE_URLS, function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions) {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->post(CC_COURSE_URLS, function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions) {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
     if(GlobalSettings::getInstance()->isCreateLinks()) {
         $post = $request->getParsedBody();
         if(is_array($post)) {
-            $course_urls = $local_functions->parseCourseUrls($post);
+            $course_urls = $ecs_local_functions->parseCourseUrls($post);
             $lectureId = $course_urls->getCmsLectureId() ?: '';
             $ecs_course_url = $course_urls->getEcsCourseUrl() ?: '';
             $lms_course_urls = $course_urls->getLmsCourseUrls();
@@ -242,28 +242,28 @@ $app->post(CC_COURSE_URLS, function (Request $request, Response $response, array
     return $response->withStatus(201);
 });
 
-$app->get(CC_COURSE_URLS, function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions) {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get(CC_COURSE_URLS, function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions) {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
 
-    return $local_functions->echoReturnFunction([], $response)->withHeader('Content-Type', 'text/uri-list');
+    return $ecs_local_functions->echoReturnFunction([], $response)->withHeader('Content-Type', 'text/uri-list');
 });
 
-$app->get('/campusconnect/course_urls/[{id}]', function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions) {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get('/campusconnect/course_urls/[{id}]', function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions) {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
 
-    return $local_functions->echoReturnFunction([], $response)->withHeader('Content-Type', 'application/json');
+    return $ecs_local_functions->echoReturnFunction([], $response)->withHeader('Content-Type', 'application/json');
 });
 
-$app->get('/campusconnect/course_urls/{id}/details', function (Request $request, Response $response, array $args) use ($db, $logging, $local_functions) {
-    if($local_functions->isValidParticipant()->isInvalidAuth()) {
+$app->get('/campusconnect/course_urls/{id}/details', function (Request $request, Response $response, array $args) use ($db, $logging, $ecs_local_functions) {
+    if($ecs_local_functions->isValidParticipant()->isInvalidAuth()) {
         return $response;
     }
 
-    return $local_functions->echoReturnFunction([], $response)->withHeader('Content-Type', 'application/json');
+    return $ecs_local_functions->echoReturnFunction([], $response)->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
